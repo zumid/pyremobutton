ca40d66bcb653d42a68be2f321e0bda96ed1844f
zumi <zumi@zumid.net>
2021-10-17 01:40:23 +0900

    [update] logger message
    ログメッセージを修正

--- pyremobutton.py
+++ pyremobutton.py
@@ -28,7 +28,7 @@ def main():
     processlist = []
     for button in settings.BUTTON.values():
         p1 = mp.Process(target=read_button_daemon, args=(button,))
-        logger.info(p1)
+        logger.info(f"[{button['NAME']}] daemon ready. {p1}")
         p1.start()
         processlist.append(p1)
     sys.exit()
@@ -36,7 +36,8 @@ def main():
 def read_button_daemon(button):
     global g_button
     g_button = button
-    logger.info("Child Process Start:NAME=%s MAC=%s COMMAND=%s",g_button['NAME'],g_button['MAC'],g_button['COMMAND'])
+    #logger.info("[%s] Child Process Start:NAME=%s MAC=%s COMMAND=%s",g_button['NAME'],g_button['NAME'],g_button['MAC'],g_button['COMMAND'])
+    logger.info(f"[{g_button['NAME']}] Child Process Start:NAME={g_button['NAME']} MAC={g_button['MAC']} COMMAND={g_button['COMMAND']}")
     dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
     bus = dbus.SystemBus()
     bus.add_signal_receiver(find_connection, bus_name="org.bluez",
@@ -44,24 +45,24 @@ def read_button_daemon(button):
                     signal_name="PropertiesChanged",
                     path_keyword="path")
     mainloop = GLib.MainLoop()
-    logger.info("Start {}".format(g_button['NAME']))
+    logger.info(f"[{g_button['NAME']}] daemon start.")
     try:
         mainloop.run()
     except KeyboardInterrupt:
-        logger.warning("KeyboardInterrupt {}".format(g_button['NAME']))
+        logger.warning(f"KeyboardInterrupt {g_button['NAME']}")
         sys.exit()
 
 def child_process(button):
     pid = os.getpid()
-    logger.info("{} event start".format(pid))
+    logger.info(f"[{g_button['NAME']}] event start pid={pid}")
     COMMAND = button['COMMAND']
     devices = [InputDevice(fn) for fn in list_devices()]
     device = ''
-    logger.info("%s,%s,%s",pid,button['NAME'],button['MAC'])
+    logger.info(f"{button['NAME']} pid={pid} MAC={button['MAC']}")
     for device in devices:
         #logger.info(pid,'[Path]',device.path,'[Name]', device.name,'[Uniq]', device.uniq)
         if button['MAC'] in str(device.uniq) and 'Control' in str(device.name):
-            logger.info("%s [find] %s",pid,device.path)
+            logger.info(f"[{g_button['NAME']}] [find] path={device.path}")
             target_device_path=device.path
             break
     del devices
@@ -71,10 +72,10 @@ def child_process(button):
     try:
         for event in dev.read_loop():
             if event.type == ecodes.EV_KEY:
-                logger.info(categorize(event))
+                logger.info(f"[{g_button['NAME']}] [categorize event] {categorize(event)}")
                 if 'down' in str(categorize(event)) :
-                    logger.info("%s %s",pid,"down is pused")
-                    logger.info("[RUN]pushed  {}".format(COMMAND))
+                    logger.info(f"[{g_button['NAME']}] pid={pid} down is pused")
+                    logger.info(f"[{g_button['NAME']}] [RUN] {COMMAND}")
                     os.system(COMMAND)
     except KeyboardInterrupt:
         logger.warning("KeyBoardInterrupt Child")
@@ -87,18 +88,19 @@ def find_connection(interface, changed, invalidated, path):
     iface = interface[interface.rfind(".") + 1:]
     logger.info("iface={} path={}".format(iface,path))
     path2 = path.replace('_',':')
-    logger.info("path2={} MAC={}".format(path2,g_button['MAC']))
+    logger.info(f"[{g_button['NAME']}] path2={path2} MAC={g_button['MAC']}")
     for name, value in changed.items():
         val = str(value)
-        logger.info("{%s.PropertyChanged} [%s] %s = %s" % (iface, path, name,val))
+        logger.info(f"[{g_button['NAME']}] {iface} PropertyChanged [{path}] {name} = {val}")
         if g_button['MAC'] in path2 and name == "Connected" and val == "1":
-            logger.info("[FIND] {}".format(g_button['NAME']))
-            logger.info("[RUN]connected  {}".format(g_button['COMMAND']))
+            logger.info(f"[{g_button['NAME']}] Bluetooth connected.")
+            logger.info(f"[{g_button['NAME']}] [RUN] with connected. {g_button['COMMAND']}")
             os.system(g_button['COMMAND'])
             g_event_process = mp.Process(target=child_process, args=(g_button,))
             g_event_process.start()
             logger.debug("##########################")
         elif g_button['MAC'] in path2 and name == "Connected" and val == "0":
+            logger.info(f"[{g_button['NAME']}] Bluetooth disconnected.")
             try:
                 g_event_process.terminate()
                 logger.debug("{} process terminated".format(gbutton['NAME']))
