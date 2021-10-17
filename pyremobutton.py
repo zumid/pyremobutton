b6b3a04c27cef44b70fb3ca3c76b08ff9907f153
zumi <zumi@zumid.net>
2021-10-17 22:18:20 +0900

    [update] New Architecture
    マルチプロセス構成からマルチスレッド構成へ変更
    設定ファイルの構成を変更
    
    [AsIs]
    
    main process
     ┣─ bruetooth monitor process (button1)
     ┃  └─ event monitor process (button1)
     ┃
     ┣─ bruetooth monitor process (button2)
     ┃  └─ event monitor process (button2)
     ┃
     └─ ...
    
    [ToBe]
    
    bluetooth monitor process
     ┣─ event monitor thread (button1)
     ┃
     ┣─ event monitor thread (button2)
     ┃
     └ ...

--- pyremobutton.py
+++ pyremobutton.py
@@ -4,7 +4,6 @@ from __future__ import absolute_import, print_function, unicode_literals
 import dbus
 import dbus.mainloop.glib
 try:
-    #from gi.repository import GObject
     from gi.repository import GLib
 except ImportError:
     exit()
@@ -12,32 +11,30 @@ import os
 import sys
 import time
 from datetime import datetime
-
-from logging import getLogger,INFO,Formatter,StreamHandler,FileHandler
-
+from logging import getLogger,Formatter,StreamHandler,FileHandler,INFO,DEBUG
 from evdev import InputDevice, categorize, ecodes, list_devices
 
-import multiprocessing as mp
+import threading
+import subprocess
 
-#from monitor_bluetooth import * 
-
-import settings
+try:
+    import settings
+except ModuleNotFoundError:
+    import settings_sample as settings
 
 
 def main():
-    processlist = []
-    for button in settings.BUTTON.values():
-        p1 = mp.Process(target=read_button_daemon, args=(button,))
-        logger.info(f"[{button['NAME']}] daemon ready. {p1}")
-        p1.start()
-        processlist.append(p1)
+    thread = {}
+    for button in settings.button_setting:
+        thread[button['name']] = threading.Thread(target=key_event_monitor,name=button['name'],args=(button,))
+        thread[button['name']].setDaemon(True)
+        thread[button['name']].start()
+
+    connection_monitor()
+
     sys.exit()
  
-def read_button_daemon(button):
-    global g_button
-    g_button = button
-    #logger.info("[%s] Child Process Start:NAME=%s MAC=%s COMMAND=%s",g_button['NAME'],g_button['NAME'],g_button['MAC'],g_button['COMMAND'])
-    logger.info(f"[{g_button['NAME']}] Child Process Start:NAME={g_button['NAME']} MAC={g_button['MAC']} COMMAND={g_button['COMMAND']}")
+def connection_monitor():
     dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
     bus = dbus.SystemBus()
     bus.add_signal_receiver(find_connection, bus_name="org.bluez",
@@ -45,73 +42,86 @@ def read_button_daemon(button):
                     signal_name="PropertiesChanged",
                     path_keyword="path")
     mainloop = GLib.MainLoop()
-    logger.info(f"[{g_button['NAME']}] daemon start.")
+    logger.info(f"commection monitor start.")
     try:
         mainloop.run()
     except KeyboardInterrupt:
-        logger.warning(f"KeyboardInterrupt {g_button['NAME']}")
+        logger.warning(f"KeyboardInterrupt of main loop")
         sys.exit()
 
-def child_process(button):
-    pid = os.getpid()
-    logger.info(f"[{g_button['NAME']}] event start pid={pid}")
-    COMMAND = button['COMMAND']
+def key_event_monitor(button):
     devices = [InputDevice(fn) for fn in list_devices()]
     device = ''
-    logger.info(f"{button['NAME']} pid={pid} MAC={button['MAC']}")
+    target_device_path=''
+    logger.info(f"[{button['name']}] MAC={button['mac']}")
     for device in devices:
-        #logger.info(pid,'[Path]',device.path,'[Name]', device.name,'[Uniq]', device.uniq)
-        if button['MAC'] in str(device.uniq) and 'Control' in str(device.name):
-            logger.info(f"[{g_button['NAME']}] [find] path={device.path}")
+        if button['mac'] in str(device.uniq) and 'Control' in str(device.name):
+            logger.info(f"[{button['name']}] InputDevice found. path={device.path}")
             target_device_path=device.path
             break
     del devices
     del device
+
+    if not target_device_path:
+        logger.warning(f"{button['name']} {button['mac']} was not found on the InputDevice. thread terminated.")
+        return
+
     dev = InputDevice(target_device_path)
     dev.capabilities(verbose=True)
     try:
         for event in dev.read_loop():
             if event.type == ecodes.EV_KEY:
-                logger.info(f"[{g_button['NAME']}] [categorize event] {categorize(event)}")
-                if 'down' in str(categorize(event)) :
-                    logger.info(f"[{g_button['NAME']}] pid={pid} down is pused")
-                    logger.info(f"[{g_button['NAME']}] [RUN] {COMMAND}")
-                    os.system(COMMAND)
+                logger.debug(f"[{button['name']}] [categorize event] {categorize(event)}")
+                if event.value == 1:    # key down
+                    start_time = time.perf_counter()
+                elif event.value == 0:  # key up
+                    pushed_time = time.perf_counter() - start_time
+                    if (pushed_time < button['long_pushed_time']):
+                        pushed='short_pushed'
+                        logger.debug(f"short push {pushed_time}")
+                        if 'KEY_VOLUMEUP' in str(categorize(event)) :
+                            run_command = button['command']['ios_button']
+                        elif 'KEY_ENTER' in str(categorize(event)):
+                            run_command = button['command']['android_button']
+                    else:
+                        pushed='long_pushed'
+                        if 'KEY_VOLUMEUP' in str(categorize(event)) :
+                            run_command = button['command']['ios_button_long']
+                        elif 'KEY_ENTER' in str(categorize(event)):
+                            run_command = button['command']['android_button_long']
+                    logger.info(f"[{button['name']}] [RUN] button={pushed} command={run_command}")
+                    try:
+                        subprocess.run(run_command,shell=True)
+                    except:
+                        logger.error(f"[{button['name']}] run failed. command=  {run_command}")
     except KeyboardInterrupt:
-        logger.warning("KeyBoardInterrupt Child")
+        logger.info("KeyBoardInterrupt. program exit.")
         sys.exit()
     sys.exit() 
 
 def find_connection(interface, changed, invalidated, path):
-    global g_button
-    global g_event_process
     iface = interface[interface.rfind(".") + 1:]
-    logger.info("iface={} path={}".format(iface,path))
+    logger.debug("iface={} path={}".format(iface,path))
     path2 = path.replace('_',':')
-    logger.info(f"[{g_button['NAME']}] path2={path2} MAC={g_button['MAC']}")
     for name, value in changed.items():
         val = str(value)
-        logger.info(f"[{g_button['NAME']}] {iface} PropertyChanged [{path}] {name} = {val}")
-        if g_button['MAC'] in path2 and name == "Connected" and val == "1":
-            logger.info(f"[{g_button['NAME']}] Bluetooth connected.")
-            logger.info(f"[{g_button['NAME']}] [RUN] with connected. {g_button['COMMAND']}")
-            os.system(g_button['COMMAND'])
-            g_event_process = mp.Process(target=child_process, args=(g_button,))
-            g_event_process.start()
-            logger.debug("##########################")
-        elif g_button['MAC'] in path2 and name == "Connected" and val == "0":
-            logger.info(f"[{g_button['NAME']}] Bluetooth disconnected.")
-            try:
-                g_event_process.terminate()
-                logger.debug("{} process terminated".format(gbutton['NAME']))
-            except NameError:
-                logger.warning("g_event_process terminate failed")
-                pass
+        logger.debug(f"{iface} PropertyChanged [{path}] {name} = {val}")
+
+        for button in settings.button_setting:
+            button_name = button['name']
+            button_mac = button['mac']
+            button_command = button['command']
+            if button_mac in path2 and name == "Connected" and val == "1":
+                logger.info(f"[{button_name}] Bluetooth Connected. [RUN] {button_command['connected']}")
+                try:
+                    subprocess.run(button_command['connected'],shell=True)
+                except:
+                    logger.error(f"[{button['name']}] run failed. command= {run_command}")
+            elif button_mac in path2 and name == "Connected" and val == "0":
+                logger.info(f"[{button_name}] Bluetooth disconnected.")
 
 if __name__ == '__main__':
-    g_button = ''
-    g_event_process = ''
-    
+
     prog_name = os.path.splitext(os.path.basename(__file__))[0]
     current_dir = os.path.dirname(__file__)
 
@@ -119,19 +129,22 @@ if __name__ == '__main__':
     logger = getLogger(__name__)
     
     # フォーマット
-    log_format = Formatter("%(asctime)s [%(levelname)8s] %(message)s")
+    log_format = Formatter("%(asctime)s [%(levelname)8s] %(funcName)s %(message)s")
     # レベル
-    logger.setLevel(INFO)
+    logger.setLevel(DEBUG)
 
     # 標準出力へのハンドラ
     stdout_handler = StreamHandler(sys.stdout)
     stdout_handler.setFormatter(log_format)
+    stdout_handler.setLevel(settings.log['log_level']['stdout'])
     logger.addHandler(stdout_handler)
+
     # ログファイルへのハンドラ
-    file_handler = FileHandler(current_dir+"/log/"+prog_name+"_"+datetime.now().strftime("%Y%m%d")+".log")
-    file_handler.setFormatter(log_format)
-    logger.addHandler(file_handler)
-    a="text"
+    if (settings.log['log_file_output']):
+        file_handler = FileHandler(current_dir+"/log/"+prog_name+"_"+datetime.now().strftime("%Y%m%d")+".log")
+        file_handler.setFormatter(log_format)
+        file_handler.setLevel(settings.log['log_level']['file'])
+        logger.addHandler(file_handler)
 
     main()
 
